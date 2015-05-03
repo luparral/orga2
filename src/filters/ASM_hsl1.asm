e3eae34d62f8e8cb730c9d4ef17b7101d13b50be
luparral <lparral@dc.uba.ar>
2015-05-03 18:25:24 -0300

    idea de suma

--- src/filters/ASM_hsl1.asm
+++ src/filters/ASM_hsl1.asm
@@ -57,7 +57,30 @@ ASM_hsl1:
 		call rgbTOhsl
 		movups xmm0, [rbp]
 		
-		;suma
+		;;;;;suma;;;;;;
+		;en xmm0 tengo [a|l|s|h]
+		;armar un registro xmm1 = [00|LL|SS|HH]
+		;sumar xmm1 y xmm0 de modo que quede xmm0 = [a|l+LL|s+SS|h+HH]
+		;copiar xmm0 en xmm7
+		;armar este registro: xmm2 = [0|1|1|360] para comparar mayor o igual
+		; ojo, el alfa puede estar mal
+		;armar este registro: xmm3 = [0|0|0|0] para comparar por menor
+		;armar este registro: xmm4 = [a|l+LL|s+SS|h+HH] (es una copia de xmm0)
+		;cmpps xmm0, xmm2, 5 (5 = not less)
+		; en xmm0 queda una mascara donde 1 = true, 0 = false
+		;armar este registro: xmm5 = [0|1|1|h+HH-360]
+		;pand xmm0, xmm5 ; en xmm0 queda donde habia ceros 0 y el valor correspondiente en xmm5 donde habia 1.
+		;armar este registro: xmm6 = [0|0|0|h+HH+360]
+		;cmpps xmm7, xmm3, 1 (1= less)
+		;en xmm7 queda una mascara donde 1 =true, 0 = false
+		;pand xmm7, xmm6 ;en xmm7 queda donde habia ceros 0 y el valor correspondiente en xmm6 donde habia 1
+		;si sumo xmm0 con xmm7 me queda todo con valores, y donde hay ceros es el else.
+		;puedo hacer un cmpps mas, con 0 y si es igual le pongo como hicimos antes una mascara que tenga los valores del else.
+		;pongo con un shuffle o algo el alfa al principio, porque creo que lo perdi
+
+		;Tengo en xmm0 el valor final procesado
+
+
 		movdqu [rbp], xmm0
 		mov rdi, rbp
 		mov rsi, rbx
