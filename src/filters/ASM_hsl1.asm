b440548bc284f5f7cc61461343aa33d50cfe3216
soviet47 <promachov@gmail.com>
2015-05-06 21:30:58 -0300

    terminado de codear el hsl1, no anda

--- src/filters/ASM_hsl1.asm
+++ src/filters/ASM_hsl1.asm
@@ -18,6 +18,8 @@ section .data
 
 comparar: dd	360.0, 1.0, 1.0, 0.0
 ceros: dd 0.0, 0.0, 0.0, 0.0
+vuelta_atras: dd -360.0, 1.0, 1.0, 0.0
+vuelta_adelante: dd 360.0, 0.0, 0.0, 0.0
 
 section .text
 ASM_hsl1:
@@ -30,20 +32,14 @@ ASM_hsl1:
 	push r13
 	push r12
 
-	lea rbx, [rbp-16]
+	lea rbx, [rbp-16]		;rbx: 16 bytes reservados para el calculo de un pixel a hsl
 
-	mov r13, rdi
-	mov r14, rsi
-	mov r15, rdx
-	movss xmm15, xmm0
-	movss xmm14, xmm1
-	movss xmm13, xmm2
-	;r13 = w
-	;r14 = h
-	;r15 = data
-	;xmm15 = hh
-	;xmm14 = ss
-	;xmm13 = ll
+	mov r13, rdi			;r13 = w
+	mov r14, rsi			;r14 = h
+	mov r15, rdx			;r15 = data
+	movss xmm15, xmm0		;xmm15 = hh
+	movss xmm14, xmm1		;xmm14 = ss
+	movss xmm13, xmm2		;xmm13 = ll
 
 
 	;calculo el tamanio de la imagen en pixeles
@@ -57,72 +53,68 @@ ASM_hsl1:
 
 
 	movdqu xmm10, [comparar]
-	movdqu xmm3, [ceros]
+	movdqu xmm11, [ceros]
+	movdqu xmm2, [vuelta_atras]
+	movdqu xmm3, [vuelta_adelante]
 	;voy a iterar cada pixel (r12) de la imagen
 	.ciclo:
 		cmp r12, 0
 		je .terminarCiclo
 
-		mov rdi, r15 		;puntero a donde empieza la imagen
+		mov rdi, r15 			;puntero a donde empieza la imagen
 		mov rsi, rbx
 
 		call rgbTOhsl
-		movups xmm0, [rbx]
+		movups xmm0, [rbx]		;xmm0 = |a|l|s|h| valores transformados a hsl
 
 		;;;;;suma;;;;;;
-		;en xmm0 tengo [a|l|s|h]
-		pxor xmm4, xmm4				;xmm4 = |00|00|00|00|
-		movss xmm4, xmm13			;xmm4 = |00|00|00|LL|
-		pslldq xmm4, 4				;xmm4 = |00|00|LL|00|
-		movss xmm4, xmm14			;xmm4 = |00|00|LL|SS|
-		pslldq xmm4, 4				;xmm4 = |00|LL|SS|00|
-		movss xmm4, xmm15			;xmm4 = |00|LL|SS|HH|
+		pxor xmm1, xmm1			;xmm1 = |00|00|00|00|
+		movss xmm1, xmm13		;xmm1 = |00|00|00|LL|
+		pslldq xmm1, 4			;xmm1 = |00|00|LL|00|
+		movss xmm1, xmm14		;xmm1 = |00|00|LL|SS|
+		pslldq xmm1, 4			;xmm1 = |00|LL|SS|00|
+		movss xmm1, xmm15		;xmm1 = |00|LL|SS|HH|
 
-		addps xmm0, xmm4			;xmm0 = |a|l+LL|s+SS|h+HH|
-		movups xmm11, xmm0          ;xmm11 = |a|l+LL|s+SS|h+HH|
-		movups xmm8, xmm0           ;xmm8 = |a|l+LL|s+SS|h+HH|
+		addps xmm0, xmm1		;xmm0 = |a|l+LL|s+SS|h+HH|
+		movups xmm7, xmm0       ;xmm7 = |a|l+LL|s+SS|h+HH|
+		movups xmm8, xmm0       ;xmm8 = |a|l+LL|s+SS|h+HH|
 
-		;;TODOOOOO
-		;;ARMAR ESOS DOS REGISTROS Y DESCOMENTAR LOS PANDS
-
-		;armar este registro: xmm5 = [0|1|1|h+HH-360]
-		;armar este registro: xmm6 = [0|0|0|h+HH+360]
+		movss xmm5, xmm0		;xmm5 = |0|0|0|h+HH|
+		addps xmm5, xmm2		;xmm5 = |0|1|1|h+HH-360|
+		movss xmm6, xmm0		;xmm6 = |0|0|0|h+HH|
+		addps xmm5, xmm3		;xmm6 = |0|0|0|h+HH+360|
 
 		cmpps xmm0, xmm10, 5
-		;pand xmm0, xmm5 ; en xmm0 queda donde habia ceros 0 y el valor correspondiente en xmm5 donde habia 1.
-		cmpps xmm11, xmm3, 1
-		;pand xmm11, xmm6 ;en xmm11 queda donde habia ceros 0 y el valor correspondiente en xmm6 donde habia 1
-		addps xmm0, xmm11
-		
-		pxor xmm11, xmm11 ;asegurarse que xmm11 no se utiliza mas
-		movups xmm11, xmm0 ;preservo xmm0
-		cmpps xmm0, xmm3, 0 ;comparo de nuevo contra 0
-		pand xmm0, xmm8 ;donde era igual a 0, pongo el resultado del else if en xmm8
+		pand xmm0, xmm5 		;en xmm0 queda donde habia ceros 0 y el valor correspondiente en xmm5 donde habia 1.
+		cmpps xmm7, xmm11, 1
+		pand xmm7, xmm6 		;en xmm7 queda donde habia ceros 0 y el valor correspondiente en xmm6 donde habia 1
+		addps xmm0, xmm7
 
-		addps xmm0, xmm11 ;en xmm0 me queda el resultado final?
+		movups xmm7, xmm0 		;preservo xmm0
+		cmpps xmm0, xmm11, 0 	;comparo de nuevo contra 0
+		pand xmm0, xmm8 		;donde era igual a 0, pongo el resultado del else if en xmm8
 
-
-
+		addps xmm0, xmm7 		;en xmm0 me queda el resultado final?
 
 		movdqu [rbx], xmm0
 		mov rdi, rbx
 		mov rsi, r15
 		call hslTOrgb
 
-		add r15, 4 ;me muevo un pixel en la imagen
-		sub r12, 4 ;decremento el contador
+		add r15, 4 				;me muevo un pixel en la imagen
+		sub r12, 1 				;decremento el contador
 		jmp .ciclo
 
 	.terminarCiclo:
 
-		pop r12
-		pop r13
-		pop r14
-		pop r15
-		pop rbx
-		add rsp, 24
-		pop rbp
-	  	ret
+	pop r12
+	pop r13
+	pop r14
+	pop r15
+	pop rbx
+	add rsp, 24
+	pop rbp
+  	ret
 
 
 
@@ -139,9 +131,9 @@ ASM_hsl1:
 		;armar este registro: xmm2 = [0|1|1|360] para comparar mayor o igual
 		; ojo, el alfa puede estar mal
 		;armar este registro: xmm3 = [0|0|0|0] para comparar por menor
-		
+
 		;armar este registro: xmm4 = [a|l+LL|s+SS|h+HH] (es una copia de xmm0)
-		
+
 		;cmpps xmm0, xmm2, 5 (5 = not less)
 		; en xmm0 queda una mascara donde 1 = true, 0 = false
 		;armar este registro: xmm5 = [0|1|1|h+HH-360]
